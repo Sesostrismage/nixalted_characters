99aa0d4e889c7d5a6e7b901223f8fe448eddd8cd
Nick Weppenaar <weppenaar@gmail.com>
2022-05-01 08:07:01 +0200

    WIP

--- character.py
+++ character.py
@@ -1,6 +1,77 @@
+import copy
 import json
+
 
 class Character:
     def __init__(self, load_path: str) -> None:
-        with open(load_path, 'r') as f:
-            self.char_dict = json.load(f)
+        with open(load_path, "r") as f:
+            self.__char_dict = json.load(f)
+
+    def get_basic_info(self):
+        return copy.deepcopy(self.__char_dict["basic_info"])
+
+    def set_character_name(self, char_name):
+        self.__char_dict["basic_info"]["character_name"] = char_name
+
+    def set_player_name(self, char_name):
+        self.__char_dict["basic_info"]["player_name"] = char_name
+
+    def get_name_info(self):
+        return copy.deepcopy(self.__char_dict["name_info"])
+
+    def set_role_name(self, role_name):
+        self.__char_dict["name_info"]["role_name"] = role_name
+
+    def set_defining_aspect(self, aspect):
+        if aspect in self.__char_dict["name_info"]["aspects"]:
+            self.remove_aspect(aspect)
+
+        self.__char_dict["name_info"]["defining_aspect"] = aspect
+
+    def add_aspect(self, aspect):
+        if self.__char_dict["name_info"]["defining_aspect"] == aspect:
+            raise KeyError(
+                f"{aspect} is already a defining aspect. Set another defining aspect first"
+            )
+
+        self.__char_dict["name_info"]["aspects"] += aspect
+
+    def remove_aspect(self, aspect):
+        self.__char_dict["name_info"]["aspects"].pop(aspect)
+
+    def get_xp(self) -> int:
+        return copy.deepcopy(self.__char_dict["name_info"]["xp"])
+
+    def set_xp(self, xp: int):
+        self.__char_dict["name_info"]["xp"] = xp
+
+    def calc_legend(self) -> int:
+        pass
+
+    def get_intimacies(self):
+        return copy.deepcopy(self.__char_dict["intimacies"])
+
+    def get_ability_names(self) -> list:
+        return sorted(list(self.__char_dict["abilities"].keys()))
+
+    def get_ability_score(self, ability_name) -> int:
+        return self.__char_dict["abilities"][ability_name]
+
+    def set_ability_score(self, ability_name, score):
+        self.__char_dict["abilities"][ability_name] = score
+
+    def calc_xp_ability_single(self, ability_name: str):
+        score = self.get_ability_score(ability_name)
+        xp = int(score * (score + 1) / 2)
+        return xp
+
+    def calc_xp_abilities_all(self):
+        xp = 0
+
+        for ability_name in self.get_ability_names():
+            xp += self.calc_xp_ability_single(ability_name)
+
+        return xp
+
+
+legend_levels = {1: 100, 2: 150, 3: 250, 4: 500, 5: 1000}
--- tests/test_character.py
+++ tests/test_character.py
@@ -1,7 +1,48 @@
 import os
 
+from character import Character
+
 test_dir = os.path.dirname(__file__)
 
 
 def test_init():
     load_path = os.path.join(test_dir, "test_char_1.json")
+    _ = Character(load_path)
+
+
+def test_get_ability_names():
+    load_path = os.path.join(test_dir, "test_char_1.json")
+    char = Character(load_path)
+    ability_name_list = char.get_ability_names()
+    assert isinstance(ability_name_list, list)
+    assert ability_name_list[0] == "athletics"
+
+
+def test_get_ability_score():
+    load_path = os.path.join(test_dir, "test_char_1.json")
+    char = Character(load_path)
+    ability_score = char.get_ability_score("athletics")
+    assert isinstance(ability_score, int)
+    assert ability_score == 4
+
+
+def test_set_ability_score():
+    load_path = os.path.join(test_dir, "test_char_1.json")
+    char = Character(load_path)
+    char.set_ability_score("athletics", 5)
+    ability_score = char.get_ability_score("athletics")
+    assert ability_score == 5
+
+
+def test_calc_xp_ability_single():
+    load_path = os.path.join(test_dir, "test_char_1.json")
+    char = Character(load_path)
+    xp = char.calc_xp_ability_single("athletics")
+    assert xp == 10
+
+
+def test_calc_xp_abilities_all():
+    load_path = os.path.join(test_dir, "test_char_1.json")
+    char = Character(load_path)
+    xp = char.calc_xp_abilities_all()
+    assert xp == 125
