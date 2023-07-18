a9237d85a293cbc41d6fa0a8da1dd14a32eaf9b7
Nick Weppenaar <weppenaar@gmail.com>
2023-07-18 13:11:49 +0200

    WIP

--- tests/test_character.py
+++ tests/test_character.py
@@ -1,49 +1,46 @@
-import os
+from pathlib import Path
+import sys
+
+from utilities import get_test_char_path
+
+# Add top-level directory to path for imports.
+sys.path.append(str(Path(__file__).parent.parent))
 
 from character import Character
 
-base_dir = os.path.dirname(__file__)
-char_dir = os.path.join(base_dir, "char_examples")
-
 
 def test_character_init():
-    load_path = os.path.join(char_dir, "test_char_1.json")
-    _ = Character(load_path)
+    _ = Character(get_test_char_path(1))
 
 
 def test_abilities_get_ability_names():
-    load_path = os.path.join(char_dir, "test_char_1.json")
-    char = Character(load_path)
+    char = Character(get_test_char_path(1))
     ability_name_list = char.abilities.get_ability_names()
     assert isinstance(ability_name_list, list)
     assert ability_name_list[0] == "athletics"
 
 
 def test_abilities_get_ability_score():
-    load_path = os.path.join(char_dir, "test_char_1.json")
-    char = Character(load_path)
+    char = Character(get_test_char_path(1))
     ability_score = char.abilities.get_ability_score("athletics")
     assert isinstance(ability_score, int)
     assert ability_score == 4
 
 
 def test_abilities_set_ability_score():
-    load_path = os.path.join(char_dir, "test_char_1.json")
-    char = Character(load_path)
+    char = Character(get_test_char_path(1))
     char.abilities.set_ability_score("athletics", 5)
     ability_score = char.abilities.get_ability_score("athletics")
     assert ability_score == 5
 
 
 def test_abilities_calc_xp_ability_single():
-    load_path = os.path.join(char_dir, "test_char_1.json")
-    char = Character(load_path)
+    char = Character(get_test_char_path(1))
     xp = char.abilities.calc_xp_ability_single("athletics")
     assert xp == 10
 
 
 def test_abilities_calc_xp_abilities_all():
-    load_path = os.path.join(char_dir, "test_char_1.json")
-    char = Character(load_path)
+    char = Character(get_test_char_path(1))
     xp = char.abilities.calc_xp_abilities_all()
     assert xp == 125
---
+++ tests/utilities.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+def get_test_char_path(char_num: int) -> Path:
+    base_dir = Path(__file__).parent
+    char_dir = base_dir / "char_examples"
+    return char_dir / f"test_char_{char_num}.json"
